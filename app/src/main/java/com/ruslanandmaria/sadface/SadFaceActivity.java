6126417d2baaffca8b374a00a308d5b139b9be4a
ruslan <ruslan.gmz@gmail.com>
2016-03-21 14:27:57 +0300

    removed redundant packages

--- app/src/main/java/com/ruslanandmaria/sadface/SadFaceActivity.java
+++ app/src/main/java/com/ruslanandmaria/sadface/SadFaceActivity.java
@@ -1,14 +1,6 @@
 package com.ruslanandmaria.sadface;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SadFaceActivity extends SingleFragmentActivity {
 
